faea60597d2b4dc9ecd1928af63799e13e3cca68
Juan Molina <jmolina@initiumsoft.com>
2024-08-24 12:12:26 -0500

    Validation integration.

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgIf,
+    NgClass
   ],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.css'
@@ -21,17 +24,27 @@ export class CreateProductComponent implements OnInit{
 
   private initializeForm() {
     this.form = this.formBuilder.group({
-      id: [],
-      name: [],
-      description: [],
-      logo: [],
-      date_release: [],
-      date_revision: []
+      id: ["", [Validators.required]],
+      name: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      logo: ["", [Validators.required]],
+      date_release: ["", [Validators.required]],
+      date_revision: ["", [Validators.required]]
     });
   }
 
   resetForm() {
     this.form.reset();
+  }
+
+  onSubmit() {
+    this.form.markAsTouched();
+    this.form.markAllAsTouched();
+    this.form.markAsDirty();
+
+    if (!this.form.valid) {
+      return;
+    }
   }
 
   ngOnInit() {
