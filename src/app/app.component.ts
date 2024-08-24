b6f72210beab884b4bbc18925008f1ea9c653178
Juan Molina <jmolina@initiumsoft.com>
2024-08-24 17:16:22 -0500

    feat: Toast view message added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {ToastComponent} from "./components/common/toast/toast.component";
+import {ToastService} from "./services/common/toast.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'challenge';
+
+  toast: { message: string, type: 'success' | 'error' | 'warning' } | null = null;
+
+  constructor(private toastService: ToastService) {}
+
+  ngOnInit(): void {
+    this.toastService.getToast().subscribe(toast => {
+      this.toast = toast;
+    });
+  }
 }
